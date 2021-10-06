29c8d3cebc1c4dfec197aea6e0fec1b05fb7e531
Gurcan Hamali <gurcan@fivefaces.com.au>
2021-10-06 21:12:54 +1100

    Sass Support

--- webpack.config.js
+++ webpack.config.js
@@ -18,14 +18,29 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: {
+                    loader: 'babel-loader',
+                    options: {
                         presets: ["@babel/preset-env", "@babel/preset-react"]
                     }
                 }
+            },
+            {
+                test:/\.scss$/,
+                use:[
+                    {
+                        loader:'style-loader'
+                    },
+                    {
+                        loader:'css-loader'
+                    },
+                    {
+                        loader:'sass-loader'
+                    }
+                ]
             }
         ]
     },
-    options: {
+    optimization: {
         minimizer: [new UglifyJsPlugin()]
     },
     plugins:[
